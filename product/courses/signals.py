5b7b8256cb3bbf7ce68382cc9e0cdacfff6f29bc
matveyvarlamov <matvejvarlamov495@gmail.com>
2024-08-21 09:29:34 +0700

    optimization signals

--- product/courses/signals.py
+++ product/courses/signals.py
@@ -7,32 +7,25 @@ from .models import Group, StudentGroup
 
 @receiver(post_save, sender=Subscription)
 def post_save_subscription(sender, instance: Subscription, created, **kwargs):
-    """
-    Распределение нового студента в группу курса.
-
-    """
-
     if created:
-        group = Group.objects.filter(course=instance.course).order_by('count_student')
+        groups = Group.objects.filter(course=instance.course).order_by('count_student')
 
-        if not group:
+        if not groups:
             new_group = Group.objects.create(title='Группа 1', count_student=1, course=instance.course)
             StudentGroup.objects.create(group=new_group, user=instance.user)
-
-        elif group.first().count_student == 0:
-            StudentGroup.objects.create(group=group.first(), user=instance.user)
-            group.first().count_student = 1
-            group.first().save()
-
-        elif group.count() < 10:
-            new_group = Group.objects.create(title=f'Группа {group.count() + 1}', count_student=1,
-                                             course=instance.course)
-            StudentGroup.objects.create(group=new_group, user=instance.user)
-
         else:
-            StudentGroup.objects.create(group=group.first(), user=instance.user)
-            group.count_student = F('count_student') + 1
-            group.save()
+            group = groups.first()
+            if group.count_student == 0 or groups.count() >= 10:
+                StudentGroup.objects.create(group=group, user=instance.user)
+                group.count_student = F('count_student') + 1
+                group.save()
+            else:
+                new_group = Group.objects.create(
+                    title=f'Группа {groups.count() + 1}',
+                    count_student=1,
+                    course=instance.course
+                )
+                StudentGroup.objects.create(group=new_group, user=instance.user)
 
     if not instance.is_valid:
         student_group = StudentGroup.objects.filter(
